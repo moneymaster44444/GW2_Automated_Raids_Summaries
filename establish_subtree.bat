4c9f98ec269f59061f2a2a572de62275f1e59297
dev_money4 <70190181+moneymaster44444@users.noreply.github.com>
2025-10-14 00:04:41 -0700

    Removing establish_subtree.bat used for one time setup to establish EI and EI Combiner as subtree.

--- establish_subtree.bat
+++
@@ -1,9 +0,0 @@
-REM Elite Insights
-git subtree add --prefix="Resources/Elite Insights" ^
-  https://github.com/baaron4/GW2-Elite-Insights-Parser.git master ^
-  --squash -m "Add EI as subtree"
-
-REM EI Combiner
-git subtree add --prefix="Resources/EI Combiner" ^
-  https://github.com/Drevarr/GW2_EI_log_combiner.git main ^
-  --squash -m "Add EI Combiner as subtree"
